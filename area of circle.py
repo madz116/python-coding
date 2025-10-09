c289e45d9930331ac36934591486dc30ebd8b711
madz116 <jcchua79@gmail.com>
2025-10-09 18:08:18 +0800

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,3 @@
+radius=float(input("Enter the radius of the circle: "))
+
+print("The area of the circle is: ",3.14*radius**2)
---
+++ average.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+c=int(input("Enter the third number: "))
+d=int(input("Enter the fourth number: "))
+e=int(input("Enter the fifth number: "))
+
+avg=a+b+c+d+e
+print("The average of the given numbers is: ",avg/5)
