a9c820c1d5fd5b9b0dc23d10e21924eaaa7f46ba
madz116 <jcchua79@gmail.com>
2025-12-04 18:09:16 +0800

    Add files via upload

---
+++ power calculation.py
@@ -0,0 +1,8 @@
+n=int(input("Enter the last number: "))
+
+for i in range(n+1):
+    print("==================")
+    print("Number: ",i)
+    print("Square: ",i*i)
+    print("Cube: ",i*i*i)
+    print("==================")
