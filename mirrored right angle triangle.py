2f2d6d8923b6eea253a78337c0fc55627ba5c31f
madz116 <jcchua79@gmail.com>
2025-12-18 18:45:38 +0800

    Add files via upload

---
+++ mirrored right angle triangle.py
@@ -0,0 +1,9 @@
+rows=int(input("Enter the number of rows: "))
+
+for i in range(1, rows+1):
+    for j in range(1, rows+1):
+        if (j<=rows-i):
+            print(" ", end=" ")
+        else:
+            print("*", end=" ")
+    print()
